8b1d774261c1c767cc3c762a42483781efa5e696
Sathvik Kandadi <sathvikkandadi@gmail.com>
2024-03-08 17:43:40 +0530

    CORS update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ interface User {
 
 const io = new Server({
     cors: {
-      origin: "http://localhost:5173"
+      origin: "*" // http://localhost:5173 if using vite * allows all
     }
   });
   
@@ -131,44 +131,5 @@ io.on("connection" ,(socket) => {
 
 
 
-// socket.on('answer', answer => {
-  //     // Handle answer from peer
-  //     socket.broadcast.emit('answer', answer);
-  // });
 
-  // socket.on('iceCandidate', candidate => {
-  //     // Handle ICE candidate from peer
-  //     socket.broadcast.emit('iceCandidate', candidate);
-  // });
-
-// import express, { Request, Response } from 'express';
-// import http from 'http';
-// import { Server , Socket } from 'socket.io';
-
-// const app = express()
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// const port = 3000
-
-// app.get('/', (req : Request, res : Response) => {
-//   res.send('Hello World!')
-// })
-
-// io.on('connection', (socket: Socket) => {
-//     console.log('A user connected');
-  
-//     // Handle chat messages
-//     socket.on('chat message', (msg: string) => {
-//       io.emit('chat message', msg);
-//     });
-  
-//     socket.on('disconnect', () => {
-//       console.log('User disconnected');
-//     });
-//   });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
 
