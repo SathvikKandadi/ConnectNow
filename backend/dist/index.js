a40d391005e205967de273c3d7ebb1a73c071ff2
Sathvik Kandadi <sathvikkandadi@gmail.com>
2024-03-08 17:51:18 +0530

    Fixed bugs while deployment

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,7 +4,7 @@ const socket_io_1 = require("socket.io");
 const RoomManager_1 = require("./managers/RoomManager");
 const io = new socket_io_1.Server({
     cors: {
-        origin: "http://localhost:5173"
+        origin: "*" // http://localhost:5173 if using vite * allows all
     }
 });
 io.listen(3000);
@@ -97,34 +97,3 @@ io.on("connection", (socket) => {
         roomManager.removeUser(socket);
     });
 });
-// socket.on('answer', answer => {
-//     // Handle answer from peer
-//     socket.broadcast.emit('answer', answer);
-// });
-// socket.on('iceCandidate', candidate => {
-//     // Handle ICE candidate from peer
-//     socket.broadcast.emit('iceCandidate', candidate);
-// });
-// import express, { Request, Response } from 'express';
-// import http from 'http';
-// import { Server , Socket } from 'socket.io';
-// const app = express()
-// const server = http.createServer(app);
-// const io = new Server(server);
-// const port = 3000
-// app.get('/', (req : Request, res : Response) => {
-//   res.send('Hello World!')
-// })
-// io.on('connection', (socket: Socket) => {
-//     console.log('A user connected');
-//     // Handle chat messages
-//     socket.on('chat message', (msg: string) => {
-//       io.emit('chat message', msg);
-//     });
-//     socket.on('disconnect', () => {
-//       console.log('User disconnected');
-//     });
-//   });
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
